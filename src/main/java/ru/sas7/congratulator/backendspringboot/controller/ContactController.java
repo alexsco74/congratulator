435b974352a7a65da32b460e334d1ba361c55a1b
Щеников Алексей <schenikov@sevastopol.softlab.ru>
2022-01-20 07:52:30 +0300

    feat(controller)[220120T0752]: Добавлен рест contact/add

--- src/main/java/ru/sas7/congratulator/backendspringboot/controller/ContactController.java
+++ src/main/java/ru/sas7/congratulator/backendspringboot/controller/ContactController.java
@@ -1,9 +1,8 @@
 package ru.sas7.congratulator.backendspringboot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.sas7.congratulator.backendspringboot.entity.Contact;
 import ru.sas7.congratulator.backendspringboot.repo.ContactRepository;
 
@@ -24,5 +23,16 @@ public class ContactController {
         List<Contact> list = contactRepository.findAll();
         System.out.println("List = " + list);
         return list;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Contact> add(@RequestBody Contact contact) {
+
+        // Проверяем чтобы не было id
+        if (contact.getId() != null && contact.getId() != 0) {
+            return new ResponseEntity("Неверный параметр: в объекте не должно быть параметра id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(contactRepository.save(contact));
     }
 }
